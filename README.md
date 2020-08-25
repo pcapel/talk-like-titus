c08693b634c5fa3ad59c65af94eda85d8f3fd125
Hashrocket Workstation <dev@hashrocket.com>
2020-08-25 11:47:38 -0500

    First pass

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Titus is the best worst dog
 
 ## Rationale
 A fun way to play with writing a Julia CLI. Why not?
+
+## TODO 
+- Generalize the selection of weights for an array of items select an element
+  from an array. Currently I am just populating the array with the number of
+  values equivalent to the weight that I want the value to have, but that's not
+  nearly elegant enough. There's probably a function written for this already.
--- src/titus.jl
+++ src/titus.jl
@@ -1,5 +1,70 @@
 module titus
+using Random
 
-greet() = print("Hello World!")
+mersenne = MersenneTwister(12346)
+# [(char, weight)]
+sparkle_chars = ['*', '*', '*', '~', '~', '~', '~', '~', '(',  ')']
+
+# he's a dog, so food emoji right?
+food_emoji = ["🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🍈", "🍒", "🍑", "🥭", "🍍",
+							"🥥", "🥝", "🍅", "🍆", "🥑", "🥦", "🥬", "🥒", "🌽", "🥕", "🧄", "🧅", "🥔", "🍠",
+							"🥐", "🥯", "🍞", "🥖", "🥨", "🧀", "🥚", "🍳", "🧈", "🥞", "🧇", "🥓", "🥩", "🍗",
+							"🍖", "🦴", "🌭", "🍔", "🍟", "🍕", "🥪", "🥙", "🧆", "🌮", "🌯", "🥗", "🥘", "🥫",
+							"🍝", "🍜", "🍲", "🍛", "🍣", "🍱", "🥟", "🦪", "🍤", "🍙", "🍚", "🍘", "🍥", "🥠",
+							"🥮", "🍢", "🍡", "🍧", "🍨", "🍦", "🥧", "🧁", "🍰", "🎂", "🍮", "🍭", "🍬", "🍫",
+							"🍿", "🍩", "🍪", "🌰", "🥜", "🍯", "🥛", "🍼", "☕️", "🍵", "🧃", "🥤", "🍶", "🍺",
+							"🍻", "🥂", "🍷", "🥃", "🍸", "🍹", "🧉", "🍾", "🧊", "🥄", "🍴", "🥣", "🥡", "🥢",
+							"🧂"]
+
+function random_bool()
+	n = rand(1)[1]
+	n > 0.5
+end
+
+function randomize_upper(s)
+	k = ""
+	for char in s
+		if random_bool()
+			k *= uppercase(char)
+		else
+			k *= char
+		end
+	end
+	return k
+end
+
+function rand_between(lower, upper)
+	n = floor(rand(1)[1] * upper)
+  while n > upper || n < lower
+		n = floor(rand(1)[1] * upper)
+	end
+	return n
+end
+
+function get_random(arr)
+	rand(mersenne, arr, 1)[1]
+end
+
+function random_emoji(str)
+	period = r"\."
+	s = str
+	while !isnothing(match(period, s))
+		replacement = get_random(food_emoji)
+		s = replace(s, "."=>replacement, count=1)
+	end
+	s
+end
+
+function sparklez()
+	sparkle_length = rand_between(5, 11)
+	sparkle = ""
+	while sparkle_length > 0
+		sparkle *= get_random(sparkle_chars)
+		sparkle_length -= 1
+	end
+	sparkle
+end
+
+println(map(x->sparklez()*random_emoji(randomize_upper(x))*sparklez(), ARGS))
 
 end # module
